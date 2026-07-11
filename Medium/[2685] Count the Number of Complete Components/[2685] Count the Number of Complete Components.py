84da7aa6e888a44996dbb11dcc5f754029296948
Samarth Jain <jsamarth84@gmail.com>
2026-07-11 12:08:05 +0530

    LeetCode-Solutions

--- Medium/[2685] Count the Number of Complete Components/[2685] Count the Number of Complete Components.py
+++ Medium/[2685] Count the Number of Complete Components/[2685] Count the Number of Complete Components.py
@@ -4,6 +4,8 @@ Accepted
 Runtime: 82 ms, faster than 26.88% of Python3 online submissions for Count the Number of Complete Components.
 Memory Usage: 18.21 MB, less than 66.51% of Python3 online submissions for Count the Number of Complete Components.
 """
+# Union-Find Solution
+# TC: O((n+m)α(n)), SC: O(n)
 class UnionFind:
     def __init__(self, n):
         self.parent = [i for i in range(n)]
@@ -50,4 +52,50 @@ class Solution:
         for root, nodes in components.items():
             if all(uf.size[root] == edges_from_node[node] + 1 for node in nodes):
                 res += 1
+        return res
+
+
+
+"""
+Accepted
+2685 [Easy]
+Runtime: 77 ms, faster than 15.37% of Python3 online submissions for Count the Number of Complete Components.
+Memory Usage: 19.60 MB, less than 90.53% of Python3 online submissions for Count the Number of Complete Components.
+"""
+# BFS Solution
+# TC: O(n+m), SC: O(n+m)
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        edgeMap = defaultdict(list)
+        for a, b in edges:
+            edgeMap[a].append(b)
+            edgeMap[b].append(a)
+
+        visited_edges = set()
+        visited_nodes = set()
+        res = 0
+        for node in range(n):
+            if node in visited_nodes:
+                continue
+            visited_nodes.add(node)
+
+            queue = deque([node])
+            node_count = 1
+            edge_count = 0
+            while queue:
+                for _ in range(len(queue)):
+                    v = queue.popleft()
+
+                    for nei in edgeMap[v]:
+                        if (v, nei) not in visited_edges and (nei, v) not in visited_edges:
+                            visited_edges.add((v, nei))
+                            edge_count += 1
+                        if nei not in visited_nodes:
+                            visited_nodes.add(nei)
+                            node_count += 1
+                            queue.append(nei)
+            
+            if edge_count == (node_count * (node_count - 1)) // 2:
+                res += 1
+
         return res
