3d17fdc474271086d82e0d7de644783d2d334c8e
Samarth Jain <jsamarth84@gmail.com>
2026-07-12 09:40:48 +0530

    LeetCode-Solutions

--- Easy/[1331] Rank Transform of an Array/[1331] Rank Transform of an Array.py
+++ Easy/[1331] Rank Transform of an Array/[1331] Rank Transform of an Array.py
@@ -1,6 +1,27 @@
 """
 Accepted
 1331 [Easy]
+Runtime: 75 ms, faster than 10.78% of Python3 online submissions for Rank Transform of an Array.
+Memory Usage: 38.62 MB, less than 10.96% of Python3 online submissions for Rank Transform of an Array.
+"""
+# Sorting Solution
+# TC: O(nlog(n)), SC: O(n)
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        N = len(arr)
+
+        sorted_arr = sorted([(num, i) for i, num in enumerate(arr)])
+        res = [0] * N
+
+        rank = 1
+        for i in range(N):
+            if i > 0 and sorted_arr[i][0] > sorted_arr[i - 1][0]:
+                rank += 1
+            res[sorted_arr[i][1]] = rank
+        return res
+
+
+
 Runtime: 243 ms, faster than 56.11% of Python3 online submissions for Rank Transform of an Array.
 Memory Usage: 33.15 MB, less than 89.62% of Python3 online submissions for Rank Transform of an Array.
 """
