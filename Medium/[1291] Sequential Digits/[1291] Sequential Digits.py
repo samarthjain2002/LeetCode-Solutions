fad20dd3bbdf593a68badc26b5184912c7705bed
Samarth Jain <jsamarth84@gmail.com>
2026-07-13 10:40:07 +0530

    LeetCode-Solutions

--- Medium/[1291] Sequential Digits/[1291] Sequential Digits.py
+++ Medium/[1291] Sequential Digits/[1291] Sequential Digits.py
@@ -1,9 +1,11 @@
 """
 Accepted
 1291 [Medium]
-Runtime: 41 ms, faster than 28.88% of Python3 online submissions for Sequential Digits.
-Memory Usage: 16.48 MB, less than 72.98% of Python3 online submissions for Sequential Digits.
+Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Sequential Digits.
+Memory Usage: 19.12 MB, less than 88.24% of Python3 online submissions for Sequential Digits.
 """
+# Sliding Window Solution
+# TC: O(1), SC: O(1)
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
         s = "123456789"
@@ -16,7 +18,34 @@ class Solution:
                 sub_string_int = int(sub_string)
                 if low <= sub_string_int <= high:
                     res.append(sub_string_int)
-
-        res.sort()
-
         return res
+
+
+
+"""
+Runtime: 0 ms, faster than 100.00% of Python3 online submissions for Sequential Digits.
+Memory Usage: 19.38 MB, less than 30.59% of Python3 online submissions for Sequential Digits.
+"""
+# Recursion + Sorting Solution
+# TC: O(1), SC: O(1)
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        res = []
+        def rec(s):
+            if not s:
+                for dig in "12345678":
+                    rec(dig)
+                return
+                
+            if int(s) > high:
+                return
+
+            if low <= int(s):
+                res.append(int(s))
+
+            dig = s[-1]
+            if dig != '9':
+                rec(s + str(int(dig) + 1))
+
+        rec("")
+        return sorted(res)
